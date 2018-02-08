1357fae6161e0b357296b5f9c7b9c643ee19a7d9
JesperVa <Smoovefan@hotmail.se>
2018-02-08 14:58:37 +0100

    Implemented basic Health system

---
+++ Game/Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public bool isAlive
+    {
+        get
+        {
+            return currentHealth > 0;
+        }
+    }
+
+    private int currentHealth;
+
+    [SerializeField]
+    private int MaxHealth = 100;
+
+	void Start ()
+    {
+        currentHealth = MaxHealth;
+	}
+	
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+    }
+
+    public void Heal(int amount)
+    {
+        //Makes sure to not overheal MaxHealth
+        currentHealth += (MaxHealth % currentHealth > amount) ? amount : MaxHealth % currentHealth;
+    }
+
+}
