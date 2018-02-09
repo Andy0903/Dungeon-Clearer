799ec1ff6bd852df9b1cac8e37f9f5dd3627f6d6
JesperVa <Smoovefan@hotmail.se>
2018-02-10 00:50:03 +0100

    Added debug code to try outfit change

--- Game/Assets/Scripts/Outfit.cs
+++ Game/Assets/Scripts/Outfit.cs
@@ -8,6 +8,11 @@ public class Outfit : MonoBehaviour
     [SerializeField]
     private SpriteRenderer[] bodyOutfit;
 
+    [SerializeField]
+    private Sprite[] body;
+
+    private int currentBodyPart;
+
     enum OutfitPiece
     {
         Head = 0,
@@ -15,6 +20,24 @@ public class Outfit : MonoBehaviour
         Body = 2,
         Hand = 3,
         Feet = 4
+    }
+
+    void Start()
+    {
+#if DEBUG
+        currentBodyPart = 0;
+#endif
+    }
+
+    void Update()
+    {
+#if DEBUG
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            bodyOutfit[(int)OutfitPiece.Body].sprite = body[currentBodyPart++ % body.Length];
+        }
+
+#endif
     }
 
     void ChangeOutfit(Sprite sprite, OutfitPiece piece)
