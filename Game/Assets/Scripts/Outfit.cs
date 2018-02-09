b75f0ebd29a93d412009f0337de9e43aebdecb1e
JesperVa <Smoovefan@hotmail.se>
2018-02-10 00:23:45 +0100

    Added script to handle outfit changes

---
+++ Game/Assets/Scripts/Outfit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Outfit : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer[] bodyOutfit;
+
+    enum OutfitPiece
+    {
+        Head = 0,
+        Hair = 1,
+        Body = 2,
+        Hand = 3,
+        Feet = 4
+    }
+
+    void ChangeOutfit(Sprite sprite, OutfitPiece piece)
+    {
+        bodyOutfit[(int)piece].sprite = sprite;
+    }
+}
