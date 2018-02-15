0a4163d8ba2114f80083e4b756c083f31df417f3
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2018-02-16 00:05:16 +0100

    Make some room prefab temps

---
+++ Game/Assets/Prefabs/Rooms/WorldBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBuilder : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] roomPrefabs;
+
+    private void Start()
+    {
+        GameObject.Instantiate(roomPrefabs[0], new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+        GameObject.Instantiate(roomPrefabs[1], new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+        GameObject.Instantiate(roomPrefabs[2], new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+    }
+}
