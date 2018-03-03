ff67b2d86f0a58c5aa0d9d43ede32cb61cf3b9bc
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2018-03-03 22:45:20 +0100

    Give player collider and rigidbody to be able to trigger triggers

--- Game/Assets/Prefabs/Rooms/WorldBuilder.cs
+++ Game/Assets/Prefabs/Rooms/WorldBuilder.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour
 {
+    public static WorldBuilder Instance { get; private set; }
+
     [SerializeField]
     GameObject[] roomPrefabs;
 
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     private void Start()
     {
-        GameObject.Instantiate(roomPrefabs[0], new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
-        GameObject.Instantiate(roomPrefabs[1], new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
-        GameObject.Instantiate(roomPrefabs[2], new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+        if(transform.childCount == 0)
+        {
+            GameObject.Instantiate(roomPrefabs[0], new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+        }
     }
 }
