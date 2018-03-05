21d168c2b8334bc33991e612087d3b41adbed27d
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2018-03-05 22:03:35 +0100

    made so rooms know where in the dungeon they are

--- Game/Assets/Prefabs/Rooms/WorldBuilder.cs
+++ Game/Assets/Prefabs/Rooms/WorldBuilder.cs
@@ -6,6 +6,8 @@ public class WorldBuilder : MonoBehaviour
 {
     public static WorldBuilder Instance { get; private set; }
 
+    Dictionary<Vector2Int, GameObject> dungeon;
+
     [SerializeField]
     GameObject[] roomPrefabs;
 
@@ -20,45 +22,71 @@ public class WorldBuilder : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+
+        dungeon = new Dictionary<Vector2Int, GameObject>();
+    }
+
+    private GameObject AddRoom(int x, int y, GameObject room, Vector3 pos)
+    {
+        GameObject go = GameObject.Instantiate(room, pos, Quaternion.identity, gameObject.transform);
+        dungeon.Add(new Vector2Int(x, y), go);
+        go.GetComponentInChildren<RoomInfo>().DungeonPosition = new Vector2Int(x, y);
+
+        return go;
     }
 
     private void Start()
     {
         if (transform.childCount == 0)
         {
-            GameObject.Instantiate(roomPrefabs[0], new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+            AddRoom(0, 0, roomPrefabs[0], new Vector3(0, 0, 0));
         }
     }
 
     public void SpawnRoom(Transform trigger, RoomExit.EDirection direction)
     {
+        Vector2Int parentDungeonPos = trigger.parent.GetComponentInChildren<RoomInfo>().DungeonPosition;
+        Vector2Int dungeonPos = Vector2Int.zero;
         RoomExit[] doorPoints = trigger.parent.GetComponentsInChildren<RoomExit>();
         int offsetX = 0;
         int offsetY = 0;
 
         switch (direction)
         {
-            case RoomExit.EDirection.North: offsetX = -1; offsetY = 0; break;
-            case RoomExit.EDirection.East: offsetX = 0; offsetY = -1; break;
-            case RoomExit.EDirection.South: offsetX = -1; offsetY = -2; break;
-            case RoomExit.EDirection.West: offsetX = -2; offsetY = -1; break;
+            case RoomExit.EDirection.North:
+                offsetX = -1;
+                offsetY = 0;
+                dungeonPos = new Vector2Int(parentDungeonPos.x, parentDungeonPos.y + 1);
+                break;
+            case RoomExit.EDirection.East:
+                offsetX = 0;
+                offsetY = -1;
+                dungeonPos = new Vector2Int(parentDungeonPos.x + 1, parentDungeonPos.y);
+                break;
+            case RoomExit.EDirection.South:
+                offsetX = -1;
+                offsetY = -2;
+                dungeonPos = new Vector2Int(parentDungeonPos.x, parentDungeonPos.y - 1);
+                break;
+            case RoomExit.EDirection.West:
+                offsetX = -2;
+                offsetY = -1;
+                dungeonPos = new Vector2Int(parentDungeonPos.x - 1, parentDungeonPos.y);
+                break;
         }
 
-        foreach (RoomExit exit in doorPoints)
+        Vector3 pos = new Vector3(trigger.position.x + offsetX, trigger.position.y + offsetY, trigger.position.z) + trigger.transform.localPosition;
+
+        //Check if there are neighboring rooms with doors etc and choose another prefab.
+
+        GameObject go = AddRoom(dungeonPos.x, dungeonPos.y, roomPrefabs[0], pos.ToVector3IntOnGrid());
+
+        RoomExit[] newDoorPoints = go.GetComponentsInChildren<RoomExit>();
+        foreach (RoomExit newExit in newDoorPoints)
         {
-            if (exit.Direction != direction)
-                continue;
-
-            Vector3 pos = new Vector3(trigger.position.x + offsetX, trigger.position.y + offsetY, trigger.position.z) + exit.transform.localPosition;
-            GameObject go = GameObject.Instantiate(roomPrefabs[0], pos.ToVector3IntOnGrid(), Quaternion.identity, gameObject.transform);
-            RoomExit[] newDoorPoints = go.GetComponentsInChildren<RoomExit>();
-
-            foreach (RoomExit newExit in newDoorPoints)
+            if (newExit.Direction == Opposite(direction))
             {
-                if (newExit.Direction == Opposite(direction))
-                {
-                    Destroy(newExit.gameObject);
-                }
+                Destroy(newExit.gameObject);
             }
         }
     }
