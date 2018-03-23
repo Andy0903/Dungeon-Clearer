4cd9108b0949203fb9195a85b03588e8d69e6492
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2018-03-23 15:04:14 +0100

    Do not activate play button in mainmenu before APIs are successfully loaded

--- Game/Assets/Scripts/Menu/MenuManager.cs
+++ Game/Assets/Scripts/Menu/MenuManager.cs
@@ -1,8 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    private void Update()
+    {
+        if (APIManager.Instance != null && APIManager.Instance.Ready)
+        {
+            GameObject play = GameObject.FindGameObjectWithTag("PlayButton");
+            play.GetComponent<Button>().interactable = true;
+            Text text = play.GetComponentInChildren<Text>();
+            text.color = new Color(text.color.r, text.color.g, text.color.b, 255); ;
+        }
+    }
+
     public void LoadScene(int id)
     {
         SceneManager.LoadScene(id);
