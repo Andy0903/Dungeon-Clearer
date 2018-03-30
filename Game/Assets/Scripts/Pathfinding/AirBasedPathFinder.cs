8b949b65ca1bc88c4de265b3cbe28f81c5a0e1bc
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2018-03-30 17:30:32 +0200

    Added some different pathfinding behaviours

---
+++ Game/Assets/Scripts/Pathfinding/AirBasedPathFinder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Pathfinder/AirBased")]
+public class AirBasedPathFinder : PathFinder
+{
+    protected override List<Vector3> FindPathBehaviour(Vector3 from, Vector3 to)
+    {
+        return new List<Vector3>() { to };
+    }
+}
