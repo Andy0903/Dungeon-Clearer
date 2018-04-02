ee4702e12094a496accb8aca0e14a1b5f926d122
JesperVa <Smoovefan@hotmail.se>
2018-04-02 18:03:42 +0200

    MovableTile created and prefab added

---
+++ Game/Assets/Scripts/MovableTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableTile : MonoBehaviour
+{
+    private GameObject tile;
+
+    public bool isPlaced
+    {
+        get;
+        private set;
+    }
+
+	void Start ()
+    {
+        tile = transform.GetChild(0).gameObject;
+        isPlaced = false;
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject == tile)
+        {
+            isPlaced = true;    
+        }
+    }
+}
