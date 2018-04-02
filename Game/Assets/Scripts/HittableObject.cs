bcc1d36018f253f745c53e2a7e32fba79e76e697
JesperVa <Smoovefan@hotmail.se>
2018-04-02 21:22:18 +0200

    Created a hittableobject

---
+++ Game/Assets/Scripts/HittableObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HittableObject : MonoBehaviour
+{
+    public bool isActivated
+    {
+        get;
+        private set;
+    }
+
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        isActivated = false;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void RegisterHit()
+    {
+        isActivated = !isActivated;
+        if (isActivated)
+        {
+            ChangeColor(Color.blue);
+        }
+        else
+        {
+            ChangeColor(Color.white);
+        }
+    }
+
+    private void ChangeColor(Color color)
+    {
+        sr.color = color;
+    }
+}
--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -125,6 +125,10 @@ public class Player : MonoBehaviour
                     Vector2 knockDirection = -(transform.position - hit.transform.position).normalized;
                     hit.collider.GetComponent<Rigidbody2D>().AddForce(knockDirection * attackKnockback);
                 }
+                else if(hit.collider.name == "HittableObject")
+                {
+                    hit.collider.GetComponent<HittableObject>().RegisterHit();
+                }
             }
         }
     }
