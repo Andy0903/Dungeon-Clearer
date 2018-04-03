4326ec33fa3408c5034ab956f623a0ed9f73ca48
JesperVa <Smoovefan@hotmail.se>
2018-04-03 20:11:08 +0200

    Created a bomb script and gameobject

---
+++ Game/Assets/Scripts/Bomb.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    [SerializeField]
+    private float radius;
+    [SerializeField]
+    private float timeToExplode;
+    [SerializeField]
+    private int damage;
+
+    private float timer;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if(timer > timeToExplode)
+        {
+            Explode();
+        }
+	}
+
+    void Explode()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, radius);
+
+        //TODO: Play a bomb animation, delete bomb after it has finished
+
+        foreach (Collider2D hit in hits)
+        {
+            Health hp = hit.gameObject.GetComponent<Health>(); //Returns null if component isn't there
+            if(hp != null) 
+            {
+                hp.DealDamage(damage);
+            }
+        }
+    }
+}
