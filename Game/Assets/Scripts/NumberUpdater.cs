68d40061de4b3e5ffa7095b4607a633d12b9ddb4
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2018-04-09 01:27:12 +0200

    Make number in mainmenu update with cleared dungeons

---
+++ Game/Assets/Scripts/NumberUpdater.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberUpdater : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameData ld = GameObject.Find("SaveLoadManager").GetComponent<SaveLoadManager>().LoadedData;
+        GetComponent<Text>().text = ld.DungeonsCleared.ToString();
+    }
+}
